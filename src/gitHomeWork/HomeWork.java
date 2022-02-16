22b6a3f4c901d8fda8aff3cd292c3328051343c5
PawelMurdza <pawel.murdza@gmail.com>
2022-02-16 13:31:04 -0500

    3rd Commit

--- src/gitHomeWork/HomeWork.java
+++ src/gitHomeWork/HomeWork.java
@@ -4,7 +4,7 @@ public class HomeWork {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Pawel Murdza Batch 12");
+System.out.println("Pawel Murdza Batch 12 is the best");
 	}
 
 }
